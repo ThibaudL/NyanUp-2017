1491448e6b57d43572f10b795f5481d2a9948ced
ThibaudL <lamarchethibaud@hotmail.fr>
2017-03-07 08:18:06 +0100

    ajout mocha pour run les tests en boucle

--- src/defis/kakawouette/BruteforceCaptcha/script.js
+++ src/defis/kakawouette/BruteforceCaptcha/script.js
@@ -1,12 +1,27 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const assert = require('assert');
+
+function myTest(){
+    return true;
+}
+
+describe('BruteforceCaptcha', function() {
+    describe('myTest()', function() {
+        it('should return true', function() {
+            assert.equal(true,myTest());
+        });
+    });
+});
+
 
 module.exports =  {
     launch : () => {
-        fetch('https://thomasknappba.wordpress.com/2014/01/15/post-crit-final-designs')
-        .then(function(res) {
-            var dest = fs.createWriteStream('./octocat.png');
-            res.body.pipe(dest);
-        });
+        // fetch('https://thomasknappba.wordpress.com/2014/01/15/post-crit-final-designs')
+        // .then(function(res) {
+        //     var dest = fs.createWriteStream('./octocat.png');
+        //     res.body.pipe(dest);
+        // });
         console.log('this is the sample')
+
     }
 };

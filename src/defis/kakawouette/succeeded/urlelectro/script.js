168d3f4a13601820cff5d4c1f750214665700205
ThibaudL <lamarchethibaud@hotmail.fr>
2017-03-08 17:07:59 +0100

    new succeeded

---
+++ src/defis/kakawouette/succeeded/urlelectro/script.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const logger = require("../../../utils/logger");
+
+const FIRST_URL = "https://callicode.fr/pydefis/UrlElectro/code/A1aA0EefCC";
+
+function searchUrl(url){
+    fetch(url)
+    .then((response) => response.text())
+    .then((text) => {
+        logger.info(text);
+        text.split(' ').forEach((potentialUrl) => {
+            if(potentialUrl.startsWith('http')){
+                searchUrl(potentialUrl);
+            }
+        })
+    });
+}
+
+module.exports =  {
+    launch : () => {
+        searchUrl(FIRST_URL);
+    }
+};

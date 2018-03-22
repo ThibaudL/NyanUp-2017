1ff75148a5dcb9d724700da03a30a389c17f5c62
atcha <francois.cornuaud@gmail.com>
2018-03-22 14:22:59 +0100

    ADD - New pydefis solved

--- src/defis/Atcha/pydefis/Herculito02Hydre/script.js
+++ src/defis/Atcha/pydefis/Herculito02Hydre/script.js
@@ -1,5 +1,20 @@
 module.exports =  {
     launch : () => {
-        console.log('This a sample');
+        let nbTete = 8542;
+        let coupEpee = 1;
+        while(nbTete > 1) {
+            coupEpee++;
+            nbTete = coupeTete(nbTete);
+        }
+
+        console.log(coupEpee);
     }
 };
+
+function coupeTete(nbTete) {
+    let newNbTete = nbTete / 2;
+    if((newNbTete)%2 != 0 && newNbTete != 1) {
+        newNbTete = newNbTete * 3 + 1;
+    }
+    return newNbTete;
+}
